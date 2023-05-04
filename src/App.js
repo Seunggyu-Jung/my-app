1bb6a776deae8a1fab1518cff65d32946b94bbe8
ThovenhairWorrior <wswm2007@naver.com>
2023-05-04 17:59:12 +0900

    5-4TIL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TripList from './components/tripList/TripList.jsx';
 
 function App() {
+  // 주석입니다!
+  /** **/  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <TripList/>
+		</div>
   );
 }
 
